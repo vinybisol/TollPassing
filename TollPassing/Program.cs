867e3a0a0a57b9851130e48ec16a7048dca06ec3
Vinicius Pedro Bisol <vinybisol@gmail.com>
2022-01-30 21:00:32 -0300

    Publish

--- TollPassing/Program.cs
+++ TollPassing/Program.cs
@@ -22,12 +22,10 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
+app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllers();
